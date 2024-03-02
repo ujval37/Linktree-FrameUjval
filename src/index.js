848d6ce60359ace2d8e9a002fa182c25b071cc86
ujval37 <122030386+ujval37@users.noreply.github.com>
2024-03-02 14:46:09 +0100

    UpdatedCode

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const path = require("path");
 const server = http.createServer((req, res) => {
   // Handle requests for the root URL "/"
   if (req.url === "/") {
-    // Read the HTML file from the src directory
-    const htmlFilePath = path.join(__dirname, "src", "InteractiveFrame.html"); // Adjust the file path accordingly
-    fs.readFile(htmlFilePath, (err, data) => {
+    // Read the HTML file from the main directory
+    const htmlFilePath = path.join(__dirname, "InteractiveFrame.html"); // Adjust the file path accordingly
+    fs.readFile(htmlFilePath, "utf-8", (err, data) => {
       if (err) {
         // If an error occurs while reading the file, return a 500 Internal Server Error
         res.writeHead(500, { "Content-Type": "text/plain" });
