9c636901e3b0664491e0318ed3cbd74ded033232
ujval37 <122030386+ujval37@users.noreply.github.com>
2024-03-02 14:08:58 +0100

    Update path

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-// Import necessary modules
-const http = require("http");
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
 
-// Create HTTP server
 const server = http.createServer((req, res) => {
   // Handle requests for the root URL "/"
   if (req.url === "/") {
-    // Read the HTML file from the file system
-    const htmlFilePath = path.join(__dirname, "InteractiveFrame.html");
+    // Read the HTML file from the main directory
+    const htmlFilePath = path.join(__dirname, "../InteractiveFrame.html"); // Adjust the file path accordingly
     fs.readFile(htmlFilePath, (err, data) => {
       if (err) {
         // If an error occurs while reading the file, return a 500 Internal Server Error
@@ -25,12 +22,4 @@ const server = http.createServer((req, res) => {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("404 Not Found");
   }
-});
-
-// Define the port
-const port = process.env.PORT || 3000;
-
-// Start the server
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
